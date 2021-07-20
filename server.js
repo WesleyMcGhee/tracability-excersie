dbfb38e4ffa9fd72222b93aa29b24848660f638b
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-20 13:58:10 -0500

    might work idk

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(rollbar.errorHandler());
 app.use(cors())
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
     rollbar.info("html file served successfully");
 })
 app.get('/api/qoutes', (req, res) => {
