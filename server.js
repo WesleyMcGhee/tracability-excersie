0fd22064788737e445e238d34ece3c3f5d320ba8
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-20 13:36:49 -0500

    work

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const ctrl = require('./controller/controller');
+const data = require('./database.json');
 const express = require('express');
 const path = require('path');
 const Rollbar = require('rollbar');
 const cors = require('cors');
+
 
 const rollbar = new Rollbar({
   accessToken: '87a929166e804d129b678da43b23cb65',
@@ -18,7 +19,9 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
     rollbar.info("html file served successfully");
 })
-app.get('/api/qoutes', ctrl.getQoute);
+app.get('/api/qoutes', (req, res) => {
+  res.status(200).send(data);
+});
 
 const port = process.env.PORT || 4000;
 
